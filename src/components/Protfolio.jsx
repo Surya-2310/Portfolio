4b93b9208c571889b6d2902cacddbb7551d73902
unknown <Email@example.com>
2026-06-08 14:40:25 +0530

    Fix Vite

--- src/components/Protfolio.jsx
+++ src/components/Protfolio.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import image from "../assets/Surya.png";
+import image from "../assets/surya.png";
 
 function Portfolio() {
   const [menuOpen, setMenuOpen] = useState(false);
